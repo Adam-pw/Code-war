7ecd5af548d54e8816721cb5906da9c1860079af
Adam-pw <adampithewan@gmail.com>
2022-12-15 13:37:24 +0530

    Preloader

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -4,11 +4,11 @@ import styles from "./Main.module.scss"
 export default function Maincomp() {
     return (
         <>
-            <h1 className={styles.head}>HI,
-                <br />WELCOME TO
-                <br />CODE-WAR CLUB.
-            </h1>
-            <p className={styles.head1}>Lorem ipsum dolor sit amet, consectetur quidem temporibus <br />magni! Possimus repellendus sapiente dolores corrupti.</p>
+                <h1 className={styles.head}>HI,
+                    <br />WELCOME TO
+                    <br />CODE-WAR CLUB.
+                </h1>
+                <p className={styles.head1}>Lorem ipsum dolor sit amet, consectetur quidem temporibus <br />magni! Possimus repellendus sapiente dolores corrupti.</p>
         </>
     )
 }
