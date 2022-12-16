92cefae7297c8e6b4662f1cb3d14523e05c6454a
Adam-pw <adampithewan@gmail.com>
2022-12-16 13:34:15 +0530

    Content changes

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from "./About.module.scss"
-import Card from "../Card/Card"
 import teams from "../../metadata/team_meta"
 import TeamGrid from "../Card/Teamgrid"
-
+import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image'
 
 export default function Aboutcomp() {
     return (
         <>
-            <h1 className={styles.head}>ABOUT US</h1>
+            <h1 className={styles.head}><b>ABOUT US</b></h1>
             <div className={styles.content}>
                 {/* <div className={styles.image}>
                      <Image
@@ -19,7 +19,7 @@ export default function Aboutcomp() {
                     /> 
                 </div> */}
                 <div className={styles.text}>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit adipisci culpa quia veniam accusamus quas illo repellendus cum aspernatur quod, facere consectetur! Ullam, cumque vel! Eaque minima, id sapiente voluptate nulla voluptas distinctio iure necessitatibus consectetur obcaecati at iste voluptates atque, recusandae autem quo molestiae in quaerat impedit suscipit corporis repudiandae, nemo cum. Voluptate eius itaque delectus ex nulla. Dolorum, laboriosam reiciendis quis, sed doloremque vero rerum totam amet et aspernatur illum saepe consectetur laudantium sequi mollitia tempora alias qui quae sapiente, quidem porro odit? Sint at illo similique? Nulla amet hic numquam molestiae aperiam quidem tenetur quibusdam esse ipsam eos. Eum ab dignissimos sint distinctio nostrum quis maiores dolores cum nam delectus! Ab vitae exercitationem culpa vero perferendis odit neque
+                    {"Greetings Everyone, Welcome to the Madhav Institute of Technology & Science, Gwalior's CodeWar Club. We are creating a community that will concentrate on learning and creating various kinds of programming and development-related initiatives. Numerous webinars, workshops, industry lectures, coding marathons, hackathons, and other events are organised by us. Our primary goal is to advance coding and programming in the college so that everyone is aware of its significance. Coding is a necessary ability in today's technologically advanced period, and all IT-related companies only hire people with this knowledge.As a result, we continuously work to promote this cause while simultaneously learning these lessons as a group. Being a member of this club will provide you the chance to learn various skills not only coding but skills such as public speaking, designing, managing events, and other relevant tasks."}
                 </div>
             </div>
             <div>
