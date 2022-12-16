ed51d3092a21bd5f2f51ffd5dc9baff37be15b94
Adam-pw <adampithewan@gmail.com>
2022-12-16 14:35:37 +0530

    final changes

--- components/Event/Event.jsx
+++ components/Event/Event.jsx
@@ -1,54 +1,12 @@
 import React from 'react'
 import styles from "./Event.module.scss"
-import Image from 'next/image'
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
 import EventCard from "../Eventcard/Eventcard.jsx";
 import events from "../../metadata/events_meta.js";
 
 export default function Eventcomp() {
     return (
         <>
-            <div className={styles.head}><b>EVENTS</b></div>
-            {/*ongoing events*/}
-            {/* <h2 className={styles.ongoing}>ONGOING EVENTS</h2>
-            <div className={styles.ongoing_banner}>
-                <Image
-                    src="/images/ongoing_banner.jpg"
-                    alt="ongoing_banner"
-                    width={900}
-                    height={900}
-
-                />
-            </div> */}
-
-            {/*past events*/}
-            {/* <h2 className={styles.past_events}>OUR PAST EVENTS</h2> */}
-
-            {/* <Carousel className={styles.carousel}>
-                <div>
-                    <Image
-                        alt='assests'
-                        src="/images/ongoing_banner.jpg"
-                        width={900}
-                        height={900} />
-                </div>
-                <div>
-                    <Image
-                        alt='assests'
-                        src="/images/ongoing_banner.jpg"
-                        width={900}
-                        height={900} />
-                </div>
-                <div>
-                    <Image
-                        alt='assests'
-                        src="/images/ongoing_banner.jpg"
-                        width={900}
-                        height={900} />
-                </div>
-            </Carousel> */}
-            <div className={styles.events}>
+            <div className={styles.head}><b>EVENTS</b></div>            <div className={styles.events}>
                 {events.map((event_card, index) => {
                     return (
                         <EventCard
