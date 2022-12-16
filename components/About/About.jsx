45b2e7f9ed573a721d1bcfb4b842ffef753ed2b1
spandita dwivedi <spandita.dwi@gmail.com>
2022-12-16 12:45:43 +0530

    About us

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -22,30 +22,126 @@ export default function Aboutcomp() {
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit adipisci culpa quia veniam accusamus quas illo repellendus cum aspernatur quod, facere consectetur! Ullam, cumque vel! Eaque minima, id sapiente voluptate nulla voluptas distinctio iure necessitatibus consectetur obcaecati at iste voluptates atque, recusandae autem quo molestiae in quaerat impedit suscipit corporis repudiandae, nemo cum. Voluptate eius itaque delectus ex nulla. Dolorum, laboriosam reiciendis quis, sed doloremque vero rerum totam amet et aspernatur illum saepe consectetur laudantium sequi mollitia tempora alias qui quae sapiente, quidem porro odit? Sint at illo similique? Nulla amet hic numquam molestiae aperiam quidem tenetur quibusdam esse ipsam eos. Eum ab dignissimos sint distinctio nostrum quis maiores dolores cum nam delectus! Ab vitae exercitationem culpa vero perferendis odit neque
                 </div>
             </div>
-            <div>
-                <h1 className={styles.heading}><b>{teams.team_heads.title}</b></h1>
+            {/* <div>
+                <h1 className={styles.heading}>{teams.team_heads.title}</h1>
                 <div className={styles.cardp}>
                     <TeamGrid team={teams.team_heads} />
                 </div>
             </div>
             <div>
-                <h1 className={styles.heading}><b>{teams.team_dev.title}</b></h1>
+                <h1 className={styles.heading}>{teams.team_dev.title}</h1>
                 <div className={styles.cardp}>
                     <TeamGrid team={teams.team_dev} />
                 </div>
             </div>
             <div>
-                <h1 className={styles.heading}><b>{teams.team_design.title}</b></h1>
+                <h1 className={styles.heading}>{teams.team_design.title}</h1>
                 <div className={styles.cardp}>
                     <TeamGrid team={teams.team_design} />
                 </div>
             </div>
             <div>
-                <h1 className={styles.heading}><b>{teams.team_all.title}</b></h1>
+                <h1 className={styles.heading}>{teams.team_all.title}</h1>
                 <div className={styles.cardp}>
                     <TeamGrid team={teams.team_all} />
                 </div>
-            </div>
+            </div> */}
+
+
+
+
+
+<h1 className={styles.text}>Team Heads</h1>
+<div className={styles.container}>
+    <Card 
+    name="Antesh Tiwari"
+    img="/images/Team/Antesh_Tiwari.jpg"
+    branch="EC"
+    github=""
+    linkedin=""
+    
+    
+    />
+    <Card
+    
+    name="Megha Tomar"
+    img="/images/Team/Megha_Tomar.jpg"
+    branch="ITIOT"
+    github=""
+    linkedin=""
+    />
+
+    </div>
+
+
+<h1 className={styles.text}>Web-Dev Team</h1>
+    <div className={styles.container}>
+    <Card
+      name="Spandita Dwivedi"
+      img="/images/Team/Spandita_dwivedi.jpeg"
+      branch="AIADS"
+      github=""
+      linkedin=""
+    
+    
+    />
+    <Card
+    
+    name="Adam Pithenwala"
+    img="/images/Team/Adam_Pithenwala.jpeg"
+    branch="AIADS"
+    github=""
+    linkedin=""
+    
+    />
+    <Card
+      name="Tanvi Shah"
+      img="/images/Team/Tanvi_Shah.jpeg"
+      branch="AIADS"
+      github=""
+      linkedin=""
+    
+    
+    
+    />
+
+    </div>
+
+
+    <h1 className={styles.text}>Team Design</h1>
+
+    <div className={styles.container}>
+    <Card
+      name="Teena Chhabra"
+      img="/images/Team/Teena_Chhabra.jpeg"
+      branch="CSD"
+      github=""
+      linkedin=""
+    
+    />
+    <Card
+     name="Naman Dubey"
+     img="/images/Team/Naman_Dubey.jpeg"
+     branch="ITIOT"
+     github=""
+     linkedin=""
+    
+    />
+
+    <Card
+     name="Abhay Sonakiyaa"
+     img="/images/Team/Abhay_Sonakiyaa.jpg"
+     branch="CSD"
+     github=""
+     linkedin=""
+    
+    
+    />
+
+    </div>
+
+
+            
         </>
     )
 }
--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,57 +1,199 @@
 import React from "react";
 import styles from "./Card.module.scss";
-import {
-  BsInstagram,
-  BsFacebook,
-  BsGithub,
-  BsGoogle,
-} from "react-icons/bs";
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return `#04${color}`;
-}
 
-export default function TeamCard({meta}) {
+export default function Cardcomp(props) {
   return (
-    <div className={styles.contaiermain}> 
-          <div className={styles.container}>
-            <img src={meta.profile_pic} alt={meta.name} className={styles.image}/>
-            <div className={styles.overlay} style={{backgroundColor: getRandomColor()}}>
-              <div className={styles.text}>
-                <div>
-                  <a href="">
-                  <BsInstagram className={styles.bsicons} />
-                  </a>
-                </div>
-                <div>
-                  <a href="">
-                  <BsFacebook className={styles.bsicons} />  
-                  </a>
-                </div>
-                <div>
-                  <a href="">
-                  <BsGithub className={styles.bsicons} />  
-                  </a>
-                </div>
-                <div>
-                  <a href="">
-                  <BsGoogle className={styles.bsicons} />  
-                  </a>
-                </div>
+    <>
+
+
+
+
+      <div className={styles.container}>
+        {/* <div className={styles.card}>
+          <div className={styles.lines}></div>
+          <div className={styles.imageBox}>
+            <img className={styles.imge} src="https://images.unsplash.com/photo-1553484771-4e29a68540f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZyUyMGV2ZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="avatar" />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.details}>
+              <h2 className={styles.head}>Alice Clooney <br /> <span className={styles.spand}>Senior Front-end Developer</span></h2>
+              <div className={styles.info}>
+                <h3 className={styles.head1}>93 <br /><span className={styles.span1}>Posts</span></h3>
+                <h3 className={styles.head1}>170k <br /><span>Followers</span></h3>
+                <h3 className={styles.head1}>842 <br /><span>Following</span></h3>
+              </div>
+              <div className={styles.actions}>
+                <button className={styles.bu1}>Follow</button>
+                <button className={styles.bu1}>Message</button>
               </div>
             </div>
           </div>
-        <div className={styles.details}> 
-          <b>{meta.name}</b>
-          <span>
-          {meta.department}
-          </span>
+        </div> */}
+
+
+
+        <div className={styles.card}>
+          <div className={styles.lines}></div>
+          <div className={styles.imageBox}>
+            <img className={styles.imge} src={props.img} alt="avatar" />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.details}>
+              <h2 className={styles.head}>{props.name} <br /> <span className={styles.spand}>{props.branch}</span></h2><br/>
+              <div className={styles.info}>
+                {/* <h3 className={styles.head1}>93 <br /><span className={styles.span1}>Posts</span></h3>
+                <h3 className={styles.head1}>170k <br /><span>Followers</span></h3>
+                <h3 className={styles.head1}>842 <br /><span>Following</span></h3> */}
+              </div>
+              <div className={styles.actions}>
+                <button className={styles.bu1}>{props.github}</button>
+                <button className={styles.bu1}>{props.linkedin}</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-  );
+
+
+
+
+      {/* <div className={styles.container}>
+        <div className={styles.card}>
+          <div className={styles.lines}></div>
+          <div className={styles.imageBox}>
+            <img className={styles.imge} src="https://images.unsplash.com/photo-1553484771-4e29a68540f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZyUyMGV2ZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="avatar" />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.details}>
+              <h2 className={styles.head}>Alice Clooney <br /> <span className={styles.spand}>Senior Front-end Developer</span></h2>
+              <div className={styles.info}>
+                <h3 className={styles.head1}>93 <br /><span className={styles.span1}>Posts</span></h3>
+                <h3 className={styles.head1}>170k <br /><span>Followers</span></h3>
+                <h3 className={styles.head1}>842 <br /><span>Following</span></h3>
+              </div>
+              <div className={styles.actions}>
+                <button className={styles.bu1}>Follow</button>
+                <button className={styles.bu1}>Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <div className={styles.lines}></div>
+          <div className={styles.imageBox}>
+            <img className={styles.imge} src="https://images.unsplash.com/photo-1553484771-4e29a68540f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZyUyMGV2ZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="avatar" />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.details}>
+              <h2 className={styles.head}>Alice Clooney <br /> <span className={styles.spand}>Senior Front-end Developer</span></h2>
+              <div className={styles.info}>
+                <h3 className={styles.head1}>93 <br /><span className={styles.span1}>Posts</span></h3>
+                <h3 className={styles.head1}>170k <br /><span>Followers</span></h3>
+                <h3 className={styles.head1}>842 <br /><span>Following</span></h3>
+              </div>
+              <div className={styles.actions}>
+                <button className={styles.bu1}>Follow</button>
+                <button className={styles.bu1}>Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={styles.card}>
+          <div className={styles.lines}></div>
+          <div className={styles.imageBox}>
+            <img className={styles.imge} src="https://images.unsplash.com/photo-1553484771-4e29a68540f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZyUyMGV2ZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="avatar" />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.details}>
+              <h2 className={styles.head}>Alice Clooney <br /> <span className={styles.spand}>Senior Front-end Developer</span></h2>
+              <div className={styles.info}>
+                <h3 className={styles.head1}>93 <br /><span className={styles.span1}>Posts</span></h3>
+                <h3 className={styles.head1}>170k <br /><span>Followers</span></h3>
+                <h3 className={styles.head1}>842 <br /><span>Following</span></h3>
+              </div>
+              <div className={styles.actions}>
+                <button className={styles.bu1}>Follow</button>
+                <button className={styles.bu1}>Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <div className={styles.lines}></div>
+          <div className={styles.imageBox}>
+            <img className={styles.imge} src="https://images.unsplash.com/photo-1553484771-4e29a68540f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZyUyMGV2ZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="avatar" />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.details}>
+              <h2 className={styles.head}>Alice Clooney <br /> <span className={styles.spand}>Senior Front-end Developer</span></h2>
+              <div className={styles.info}>
+                <h3 className={styles.head1}>93 <br /><span className={styles.span1}>Posts</span></h3>
+                <h3 className={styles.head1}>170k <br /><span>Followers</span></h3>
+                <h3 className={styles.head1}>842 <br /><span>Following</span></h3>
+              </div>
+              <div className={styles.actions}>
+                <button className={styles.bu1}>Follow</button>
+                <button className={styles.bu1}>Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <div className={styles.lines}></div>
+          <div className={styles.imageBox}>
+            <img className={styles.imge} src="https://images.unsplash.com/photo-1553484771-4e29a68540f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZyUyMGV2ZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="avatar" />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.details}>
+              <h2 className={styles.head}>Alice Clooney <br /> <span className={styles.spand}>Senior Front-end Developer</span></h2>
+              <div className={styles.info}>
+                <h3 className={styles.head1}>93 <br /><span className={styles.span1}>Posts</span></h3>
+                <h3 className={styles.head1}>170k <br /><span>Followers</span></h3>
+                <h3 className={styles.head1}>842 <br /><span>Following</span></h3>
+              </div>
+              <div className={styles.actions}>
+                <button className={styles.bu1}>Follow</button>
+                <button className={styles.bu1}>Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={styles.card}>
+          <div className={styles.lines}></div>
+          <div className={styles.imageBox}>
+            <img className={styles.imge} src="https://images.unsplash.com/photo-1553484771-4e29a68540f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZyUyMGV2ZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="avatar" />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.details}>
+              <h2 className={styles.head}>Alice Clooney <br /> <span className={styles.spand}>Senior Front-end Developer</span></h2>
+              <div className={styles.info}>
+                <h3 className={styles.head1}>93 <br /><span className={styles.span1}>Posts</span></h3>
+                <h3 className={styles.head1}>170k <br /><span>Followers</span></h3>
+                <h3 className={styles.head1}>842 <br /><span>Following</span></h3>
+              </div>
+              <div className={styles.actions}>
+                <button className={styles.bu1}>Follow</button>
+                <button className={styles.bu1}>Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div> */}
+    </>
+  )
 }
