51d8f8a22ba0b68d8e8b96049083cf2e94a116c0
Ankur Sharma <123960328+Sharma-Ankur-01@users.noreply.github.com>
2024-02-03 16:38:59 +0530

    Update team_meta.js

--- metadata/team_meta.js
+++ metadata/team_meta.js
@@ -6,7 +6,6 @@ const team_heads = {
       name: "Antesh Tiwari",
       profile_pic: `${image_path}/Antesh_Tiwari.jpg`,
       department: "EC",
-      position:"President",
       linkedIn: "",
       instagram: "",
       external: "",
